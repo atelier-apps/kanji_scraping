a7fafe46c79ed3d332c5f61d9d3beb3ff1e9350d
yoshy-toshy <yoshinaga.toshihiro@gmail.com>
2019-09-02 02:10:48 +0900

    漢音取得

--- dictionaryTest.py
+++ dictionaryTest.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 
 # アクセスするURL
 url = "https://zh.wiktionary.org/wiki/%E9%99%84%E5%BD%95:%E7%8E%B0%E4%BB%A3%E6%B1%89%E8%AF%AD%E5%B8%B8%E7%94%A8%E5%AD%97%E8%A1%A8"
-base_url="https://zh.wiktionary.org"
 
 html = urllib.request.urlopen(url=url)
 
@@ -19,8 +18,8 @@ for a_tag in a_tags:
     if len(title)!=1:
         continue
     href = a_tag["href"]
-    text+=title+"\t"+base_url+href+"\n";
+    text+=title+"\n";
 
 
-with open("./kanji_list.txt", mode='w') as f:
+with open("./hanzi_list.txt", mode='w') as f:
     f.write(text)
